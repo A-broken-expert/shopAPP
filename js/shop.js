c36b80d945897d56b73bacf799b24dc7d200f721
A-broken-expert <1637910493@qq.com>
2018-09-18 20:14:59 +0800

    k

--- js/shop.js
+++ js/shop.js
@@ -5,9 +5,28 @@ window.onload=function(){
     let gou=document.querySelector("footer .shop");
     let ge=document.querySelector("footer .price .top");
     let price=document.querySelector("footer .sure");
-    let a=0,b=0,c=0,d=0,e=0,f=0,j=0,h=0;
-    let arr=[a,b,c,d,e,f,j,h];
+    let arr=[0,0,0,0,0,0,0,0];
     let arr1=[5,10,15,20,25,30,35,40];
+    // 获取遮盖
+    let cover=document.querySelector(".appCover");
+    // 获取遮盖减号
+    let coverJian=document.querySelector(".appCover .jianBth");
+    // 获取遮盖加号
+    let coverJia=document.querySelector(".appCover .jiaBth");
+    // 获取遮盖之间的数量
+    let coverShu=document.querySelector(".appCover .jaz");
+    // 获取合计
+    let coverHj=document.querySelector(".appCover .zon span");
+    // 获取确认按钮
+    let coverSubmit=document.querySelector(".appCover .zon .button");
+    // 获取取消按钮
+    let coverErr=document.querySelector(".appCover i");
+
+    console.log(coverErr);
+
+    zhis.forEach((v)=>{
+        v.style.display="none";
+    });
     // 点击加号
     jias.forEach((value,index)=>{
         value.onclick=function(){
@@ -19,16 +38,19 @@ window.onload=function(){
             let zj=0;
             arr.forEach((v,i)=>{
                 shu += v;
-                zj+=v*arr1[i];
+                zj += v*arr1[i];
             });
             if(zj >= 20){
                 price.style.background="linear-gradient(to right,#ffff00,#ff6d00)";
                 gou.style.background="linear-gradient(to right,#ffff00,#ff6d00)";
-                price.onclick=function(){
-                    window.open("Password payment.html")
-                };
+
                 ge.innerHTML=`已选购${shu}商品`;
                 price.innerHTML=`￥${zj}`;
+                coverShu.innerHTML=`${shu}`;
+                coverHj.innerHTML=`合计：${zj}RMB`;
+                price.onclick=function(){
+                    cover.style.transform="scale(1)";
+                };
             }
         }
     });
@@ -43,16 +65,23 @@ window.onload=function(){
             arr.forEach((v,i)=>{
                 shu += v;
                 zj += v*arr1[i];
-
             });
             if(zj<20){
                 price.style.background="#000";
                 gou.style.background="#ccc";
+                ge.innerHTML=`未选购商品`;
+                price.innerHTML=`￥ 20元起送`;
             }else if(zj>=20){
+                price.style.background="linear-gradient(to right,#ffff00,#ff6d00)";
+                gou.style.background="linear-gradient(to right,#ffff00,#ff6d00)";
                 price.onclick=function(){
-                    window.open("Password payment.html")
+                    cover.style.transform="scale(1)";
+                    coverShu.innerHTML=`${shu}`;
+                    coverHj.innerHTML=`合计：${zj}RMB`;
+
                 };
                 ge.innerHTML=`已选购${shu}商品`;
+
                 price.innerHTML=`￥${zj}`;
             }
             arr.forEach((e,d)=>{
@@ -68,6 +97,12 @@ window.onload=function(){
             });
         }
     });
+    coverErr.onclick=function(){
+        cover.style.transform="scale(0)";
+    };
+    coverSubmit.onclick=function () {
+        window.open("Password payment.html");
+    }
 
 };
 
